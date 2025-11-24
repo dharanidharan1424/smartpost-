180e28db7f9fd9c616136e2f64653ba2ce8e995c
dharanidharan1424 <leadsbydharan@gmail.com>
2025-11-24 17:17:41 +0530

    Initial commit from Create Next App

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    domains: ["res.cloudinary.com", "via.placeholder.com"],
+  },
+  reactCompiler: true,
+};
+
+export default nextConfig;
